2b98a396651373c3882bc3e98ad79edc9ba3a609
hoshikoo <hoshikooki@Hoshikos-iMac.local>
2014-09-18 00:03:30 -0400

    091814

--- app.js
+++ app.js
@@ -1,99 +1,130 @@
-$(document).ready(function() {
-	// This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
-  }
+$(document).ready(function(){
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+	var getResult = function(artistNameTag){
 
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '{340356569469032}',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.1' // use version 2.1
-  });
+		// var artistNameTag = "jimi+henrdix";	
 
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
+			$.getJSON("http://developer.echonest.com/api/v4/artist/search?api_key=HDRBCYY9TLSIFUMSR&format=json&name="+artistNameTag+"&results=1", function(data) { 
+			var responseData = data.response
+			$(".artist-name").text(responseData["artists"][0]["name"]);
+			});
 
 
+			$.getJSON("http://developer.echonest.com/api/v4/artist/biographies?api_key=HDRBCYY9TLSIFUMSR&name="+artistNameTag+"&format=json&results=1&start=0&license=cc-by-sa", function(data) { 
+			var responseData = data.response
+			$(".bio").text("Bio: " + responseData["biographies"][0]["text"]);
+			});
+
+			$.getJSON("http://developer.echonest.com/api/v4/artist/images?api_key=HDRBCYY9TLSIFUMSR&name="+artistNameTag+"&format=json&results=5&start=0&license=unknown", function(data) { 
+				var responseData = data.response
+				$(".pics").attr("src", responseData["images"][0]["url"]); 
+				$(".pics").click(function(){
+					if(this.src == responseData["images"][0]["url"]){
+				        this.src = responseData["images"][1]["url"];
+				    } else if(this.src == responseData["images"][1]["url"]){
+				    	 this.src = responseData["images"][2]["url"];
+				    }else if(this.src == responseData["images"][2]["url"]){
+				    	 this.src = responseData["images"][3]["url"];
+				    }else if(this.src == responseData["images"][3]["url"]){
+				    	 this.src = responseData["images"][4]["url"];
+				    }else {
+				        this.src = responseData["images"][0]["url"];
+				    }				
+				});
+
+				// $('.pictures').find('img').each(function(){
+				//   var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+				//   $(this).addClass(imgClass);
+				//  });
+
+				var artistpic = $('.pics').height();
+			    if (artistpic < 250) {
+			        var margintop = (250 - artistpic) / 2;
+			        $('.pics').css('margin-top', margintop);
+			    }
+			});
 
 
-	$.getJSON("https://graph.facebook.com/search?q=king+street&[type={user}](#user)", function(data) { 
-		console.log(data)
+			$.getJSON("http://developer.echonest.com/api/v4/artist/terms?api_key=HDRBCYY9TLSIFUMSR&name="+artistNameTag+"&format=json", function(data) { 
+			var responseData = data.response
+			$(".genre").text("Genre: " + responseData["terms"][0]["name"] + ", "+ responseData["terms"][1]["name"]+ ", "+ responseData["terms"][2]["name"] ); 
+			});
+
+			$.getJSON("http://developer.echonest.com/api/v4/artist/songs?api_key=HDRBCYY9TLSIFUMSR&name="+artistNameTag+"&format=json&start=0&results=2", function(data) { 
+			var responseData = data.response
+			console.log(responseData["songs"][0]["title"]); 
+			});
+
+			$.getJSON("http://developer.echonest.com/api/v4/artist/news?api_key=HDRBCYY9TLSIFUMSR&name="+artistNameTag+"&format=json&results=1&start=0", function(data) { 
+			var responseData = data.response
+			$(".title").text("Related News: "+responseData["news"][0]["name"]); 
+			$(".url").text("Link: "+responseData["news"][0]["url"]); 
+			$(".summary").html("Summsry: "+ responseData["news"][0]["summary"]); 
+			});
+
+			$.getJSON("http://developer.echonest.com/api/v4/artist/urls?api_key=HDRBCYY9TLSIFUMSR&name="+artistNameTag+"&format=json", function(data) { 
+			var responseData = data.response
+			$(".officialsite").text(responseData["urls"]["official_url"]); 
+			$(".officialsite a").attr("href", responseData["urls"]["official_url"]); 
+			});
+
+			$.getJSON("http://developer.echonest.com/api/v4/artist/similar?api_key=HDRBCYY9TLSIFUMSR&name="+artistNameTag+"&format=json&results=5&start=0", function(data) { 
+			var responseData = data.response
+			$(".simiName").text(responseData["artists"][0]["name"]); 
+				var similarid = responseData["artists"][0]["id"];
+				$.getJSON("http://developer.echonest.com/api/v4/artist/images?api_key=HDRBCYY9TLSIFUMSR&id="+similarid+"&format=json&results=1&start=0&license=unknown", function(data) { 
+						var responseData = data.response
+						$(".simipics").attr("src", responseData["images"][0]["url"]); 
+					});
+				$(".simipics").click(function(){
+					//do the search of the similar artist
+					var artistName =responseData["artists"][0]["name"];
+					$(".artist-name").html('');
+					event.preventDefault();
+					var artistNameTag = artistName.toLowerCase().split(' ').join('+');
+					getResult(artistNameTag);
+				});
+			});
+			
+
+		};
+
+
+// alert(artistNameTag);
+// getResult;
+
+	$('.artistSubmit').submit( function(event){
+		// get the value of the tags the user submitted
+		var artistName = $(this).find("input[name='artistName']").val();
+		// getUnanswered(tags);
+		$(".artist-name").html('');
+		// artistName = artistName.toTitleCase();
+		// $(".artist-name").text(artistName);
+		event.preventDefault();
+
+		var artistNameTag = artistName.toLowerCase().split(' ').join('+');
+
+		alert(artistNameTag);
+		getResult(artistNameTag);
 	});
-
-	$.getJSON("https://graph.facebook.com/kingstsounds?callback=?", function(data) { 
-	alert("pageID: " + data.id); 
-	alert("Likes: " + data.likes); 
-	alert("Likes: " + data.link); 
-	alert("Likes: " + data.name); 
-
-
-	});
-
-
 
 });
+
+		
+// 		$('.artistSubmit').submit( function(event){
+// 		// zero out results if previous search has run
+// 		$('.results').html('');
+// 		// get the value of the tags the user submitted
+// 		var artistName = $(this).find("input[name='artistName']").val();
+// 		getTopAnsweres(artistName);
+// 	    // $('.results').each(function(){ $(this).append($(this).index()) });
+// 	});
+	
+// });
+
+// var result = $.ajax({
+// 		url: "http://developer.echonest.com/api/v4/artist/similar?api_key=HDRBCYY9TLSIFUMSR&"+artistName,
+// 		data: request,
+// 		dataType: "jsonp",
+// 		type: "GET",
+// 		})
